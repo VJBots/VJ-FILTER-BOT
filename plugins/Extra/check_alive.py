9cc7018139d33a0c34449e26fa6b9433f7a87f72
RIO NETWORKS <122881477+1238muj@users.noreply.github.com>
2023-07-31 13:08:08 +0530

    Add files via upload

---
+++ plugins/Extra/check_alive.py
@@ -0,0 +1,18 @@
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("you very luck 🤞 iam alive ❤️ press /start use me")
+
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
