cb288e9437b832edd6067df21acee92bba117944
RIO NETWORKS <122881477+1238muj@users.noreply.github.com>
2023-08-05 20:06:07 +0530

    Update tts.py

--- plugins/Extra/tts.py
+++ plugins/Extra/tts.py
@@ -3,7 +3,6 @@ import traceback
 from asyncio import get_running_loop
 from io import BytesIO
 
-from googletrans import Translator
 from gtts import gTTS
 from pyrogram import Client, filters
 from pyrogram.types import Message
