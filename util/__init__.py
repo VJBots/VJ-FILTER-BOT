6fc538329cbc268b02dae7a626865d4a3349cd12
RIO NETWORKS <122881477+TEAM-RIO-NETWORKS@users.noreply.github.com>
2023-08-29 09:07:47 +0530

    Add files via upload

---
+++ util/__init__.py
@@ -0,0 +1,1 @@
+# I hate my Dirty-Mind @LazyDeveloperr & @creatorrio 😎😍
---
+++ util/keepalive.py
@@ -0,0 +1,22 @@
+#Thanks @Lazydeveloperr helping this journey 
+import asyncio
+import logging
+import aiohttp
+import traceback
+from info import *
+
+
+async def ping_server():
+    sleep_time = PING_INTERVAL
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(URL) as resp:
+                    logging.info("Pinged server with response: {}".format(resp.status))
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
