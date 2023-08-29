e4f1930932a14e49998a3337e0b882b8b4dfaff8
RIO NETWORKS <122881477+TEAM-RIO-NETWORKS@users.noreply.github.com>
2023-08-29 09:09:06 +0530

    Add files via upload

---
+++ server/exceptions.py
@@ -0,0 +1,10 @@
+
+    # 🥰  Thank you for helping  this journey  @LazyDeveloperr  🥰
+    # thanks Adarsh Goel for Server Routing
+    #thanks @creatorrio
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
