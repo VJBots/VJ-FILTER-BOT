ac53a2276089ebeff1558c6733aee73c59c027a0
RIO NETWORKS <122881477+TEAM-RIO-NETWORKS@users.noreply.github.com>
2023-09-02 20:11:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,6 +79,7 @@ async def start(client, message):
             [
                 InlineKeyboardButton(
                     "❆ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link
+                InlineKeyboardButton(,🤔 Why Iam Join', callback_data='joinforce')
                 )
             ]
         ]
