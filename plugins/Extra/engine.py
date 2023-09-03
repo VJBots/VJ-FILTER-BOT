c36d2e77b6590e984771e83950cea3d48c4c2821
RIO NETWORKS <122881477+TEAM-RIO-NETWORKS@users.noreply.github.com>
2023-09-03 09:25:04 +0530

    Update engine.py

--- plugins/Extra/engine.py
+++ plugins/Extra/engine.py
@@ -1,7 +1,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "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" #Your openai api key
+    openai.api_key = "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" #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
