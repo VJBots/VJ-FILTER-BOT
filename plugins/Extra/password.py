7d482832767e836f78bef44a6dc599c0d6566b88
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-09-19 12:27:39 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Team Rio™️', url='https://t.me/Teamrioupdates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('VJ Bots', url='https://t.me/vj_bots')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
