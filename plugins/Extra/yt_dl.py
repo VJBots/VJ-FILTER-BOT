ca81001cf92cf307338a442a5820640b0fe4b30d
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-09-19 12:32:25 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [RIO NETWORKS™](https://t.me/creatorrio)**"
+        cap = "**BY›› [VJ NETWORKS™](https://t.me/vj_bots)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
