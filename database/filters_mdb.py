604e1b4367fe6a47a523a34c90a2e5fc3f1697d1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-02 10:50:15 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import pymongo
 from info import DATABASE_URI, DATABASE_NAME
 from pyrogram import enums
