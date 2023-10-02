128777e9f87c587d283a34cd92500089efdf4405
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-02 10:16:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
