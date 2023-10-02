77eeb2dc6936540a4a8c6192ab875985c54a9418
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-02 10:45:33 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import re
 from pyrogram import filters, Client, enums
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
