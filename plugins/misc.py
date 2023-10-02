56dc44414e1d3298880c2942084fd252b4218cbe
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-02 10:46:44 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
