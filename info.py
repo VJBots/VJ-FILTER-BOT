6aeb955aaa461655d2cb2b35c8ba1b7ee9e3455a
ALAS <139692597+YTNOW@users.noreply.github.com>
2023-10-05 14:31:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,10 +53,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdiskurl.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'ebec5142e7936b68e7e59f6e29c929960e010031')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
