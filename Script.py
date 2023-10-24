0f398a876d44673ba798675a4647d26c4cb18039
vamshiworld <128972919+vamshiworld@users.noreply.github.com>
2023-10-24 19:42:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : 
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {{file_name}}
 
 <b> Size ⚙️: {file_size}</b>""" 
 
