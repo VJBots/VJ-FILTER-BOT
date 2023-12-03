bc6d552175b3cb83693b09628bd7abfd2ee9a63d
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-12-03 08:42:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,11 +1,11 @@
 from os import environ
 
 
-API_ID = environ.get("API_ID", "27187191")
-API_HASH     = "f32586203fhah814ea949ad984839c61"
-BOT_TOKEN    = "6468147517:AAEoAnKdYeo-Gdos-SeMbNT7tC-wJrK0"
-SESSION      = "BQGeyJ0AbtuhGfuqu_73Gg_PdYg5YOXq7GdEP2ufM_QiZzabygEqIfw70vyYvgYuKxwf-Jp9IYqmJLmzkPDwqDONy17X4Xlh69HCzsuwVQMJQEiPmkhO0h2pSkvXRN9E0HGzX7WHPi4EinShrtyaJ8soOu0AT453wqBy6Ns3dDH1tPM---Tk1rsQ--n3fR1DzOSGapxBfJmQGXZJPijrJajM8KaE9ekNH_5QKRfx-cZavQAdGha-XiAGt0GwxCiGYK1OLs2f3RIFppX3t68wyBNL3CnGwAAAAFMBNksAA"
-DATABASE_URI = "mongodb+srv://hsjsh:haja@cluster0.nqjle5f.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1002081995090
-ADMIN        = 5570372452
-CHANNEL      = "@VJ_Bots"
+API_ID = int(environ.get("API_ID", "27187191"))
+API_HASH = environ.get("API_HASH", "f32586203fhah814ea949ad984839c61")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6468147517:AAEoAnKdYeo-Gdos-SeMbNT7tC-wJrK0")
+SESSION = environ.get("SESSION", "BQGeyJ0AbtuhGfuqu_73Gg_PdYg5YOXq7GdEP2ufM_QiZzabygEqIfw70vyYvgYuKxwf-Jp9IYqmJLmzkPDwqDONy17X4Xlh69HCzsuwVQMJQEiPmkhO0h2pSkvXRN9E0HGzX7WHPi4EinShrtyaJ8soOu0AT453wqBy6Ns3dDH1tPM---Tk1rsQ--n3fR1DzOSGapxBfJmQGXZJPijrJajM8KaE9ekNH_5QKRfx-cZavQAdGha-XiAGt0GwxCiGYK1OLs2f3RIFppX3t68wyBNL3CnGwAAAAFMBNksAA")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://hsjsh:haja@cluster0.nqjle5f.mongodb.net/?retryWrites=true&w=majority")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002081995090"))
+ADMIN = int(environ.get("ADMIN", "5570372452"))
+CHANNEL = "@VJ_Bots"
