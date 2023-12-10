bc18d21019186590c050edb546aaea50e312666f
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-12-10 10:07:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 import sys
 import glob
 import importlib
@@ -36,22 +35,22 @@ from plugins import web_server
 
 import asyncio
 from pyrogram import idle
-from lazybot import LazyPrincessBot
+from bot import VJBot
 from util.keepalive import ping_server
-from lazybot.clients import initialize_clients
+from bot.clients import initialize_clients
 
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-LazyPrincessBot.start()
+VJBot.start()
 loop = asyncio.get_event_loop()
 
 
-async def Lazy_start():
+async def start():
     print('\n')
-    print('Initalizing Lazy Bot')
-    bot_info = await LazyPrincessBot.get_me()
-    LazyPrincessBot.username = bot_info.username
+    print('Initalizing VJ Bot')
+    bot_info = await VJBot.get_me()
+    VJBot.username = bot_info.username
     await initialize_clients()
     for name in files:
         with open(name) as a:
@@ -63,18 +62,18 @@ async def Lazy_start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Lazy Imported => " + plugin_name)
+            print("Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
-    me = await LazyPrincessBot.get_me()
+    me = await VJBot.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    LazyPrincessBot.username = '@' + me.username
+    VJBot.username = '@' + me.username
     logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
@@ -82,7 +81,7 @@ async def Lazy_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await VJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
@@ -92,7 +91,7 @@ async def Lazy_start():
 
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(Lazy_start())
+        loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
 
