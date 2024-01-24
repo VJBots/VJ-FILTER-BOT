b0545f093a7571e472cdc73d209098d5e03c9554
Yash <153117195+Naitik4singh@users.noreply.github.com>
2024-01-24 12:39:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,10 +53,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "YSbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'krishnalink.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '16109001fbf4b94df74eb6fb16b5c2f33d33eaaf')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
